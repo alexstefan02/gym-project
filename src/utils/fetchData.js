13c7b4d349e1dcf49c084ba092ffd1eadc2b2075
Ștefan Alexandru <alexandrustefan450@gmail.com>
2022-07-06 11:48:35 +0300

    Updated

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+    method: 'GET',
+  
+  headers: {
+    'X-RapidAPI-Key': "578e16c617msh6d83a2d985e133fp1d0359jsna12eb7b88834",
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+}
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data= await response.json();
+
+    return data;
+}
